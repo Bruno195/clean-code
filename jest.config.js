7dbe50ec64ed2abee218c62899f45075671527ac
Bruno195 <brunomonteiro195@gmail.com>
2023-05-04 03:54:33 -0300

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,14 @@
+/* eslint-disable no-useless-escape */
 /*
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
  */
 
-export default {
-  roots: ['<rootDir>/src'],
+module.exports = {
+  roots: ['<rootDir>\\src'],
   collectCoverage: false,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  // eslint-disable-next-line no-useless-escape
+  collectCoverageFrom: ['<rootDir>\\src\\**\\*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
